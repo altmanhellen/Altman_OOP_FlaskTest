23924f74c1e2bc67debd4992ea715f1781150bbb
Elena Altman <altmanhellen@gmail.com>
2022-10-16 20:43:53 +0300

    Added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from operator import add, mul, pow, sub, truediv
 from string import ascii_letters
 import string
+import csv
 
 
 app = Flask(__name__)
@@ -27,6 +28,19 @@ def projects():
 @app.route('/contacts')
 def contacts():
     return render_template('contacts.html')
+
+
+@app.route('/cities')
+def printCities():
+    file = open(
+        '/Users/helene/Library/Mobile Documents/com~apple~CloudDocs/Desktop/Python/Домашнее задание/Russian_cities.csv',
+        'r')
+    data = file.read()
+    words = data.split('\n')
+    length = len(words)
+    for i in range(len(words)):
+        print(words[i])
+    return render_template('cities.html', words=words, length=length)
 
 
 if __name__ == '__main__':
